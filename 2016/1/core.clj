c085f8c6b4f93a1cb8bfd480ff36cbbfe3393d9a
Kennet Persson <kennet@slashx.se>
2016-12-01 16:31:27 +0100

    Första uppgiften för 1:a december löst

--- 2016/1/core.clj
+++ 2016/1/core.clj
@@ -1,13 +1,24 @@
+(ns AOC-2016-1.core (:require [clojure.string]))
+
 (def instructions (slurp "instructions.txt"))
 
-(defn turn [m dx dy] [dx dy])
+(defn turn [m dx dy]
+    (case (first m)
+     \R [dy (- dx)]
+     \L [(- dy) dx]))
 
-(defn dist [m] 1)
+(defn dist [m] (int (subs m 1)))
 
 (defn move [[dx dy x y] m]
     (let [[ndx ndy] (turn m dx dy)
           d (dist m)]
-        [ndx ndy (+ x (* d ndx)) (+ y (* d ndy)]))
+        [ndx ndy (+ x (* d ndx)) (+ y (* d ndy))]))
 
-(reduce move [0 1 0 0] (split instructions #", "))
+(def end (reduce move [0 1 0 0] (clojure.string/split instructions #", ")))
+
+(defn abs [n] (max n (- n)))
+
+(println (str "Answer 1: " (let [[_ _ x y] end] (+ (abs x) (abs y)))))
+
+  
 
