263315eeef483d64b003f10e52588d9a5a652fd9
Kennet Persson <kennet@slashx.se>
2016-12-02 17:40:57 +0100

    Lite kodat på första uppgiften

--- 2016/2/core.clj
+++ 2016/2/core.clj
@@ -1,3 +1,17 @@
 (ns AOC-2016-2.core (:require [clojure.string]))
 
 (def instructions (slurp "instructions.txt"))
+
+(def lines (clojure.string/split-lines instructions))
+
+(defn move [[x y] m]
+    (case m
+        \U [x (max 0 (dec y))]
+        \D [x (min 2 (inc y))]
+        \L [(max 0 (dec x)) y]
+        \R [(min 2 (inc x)) y]))
+
+(defn code [x y]
+    (+ 1 (* 3 y) x))
+
+    
