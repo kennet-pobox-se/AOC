c6b673cd03fb8c3699f556999826da265b8a2f73
Kennet Persson <kennet@pobox.se>
2016-12-07 23:24:14 +0100

    3:e decembers första uppgift löst

---
+++ 2016/3/core.clj
@@ -0,0 +1,22 @@
+(ns AOC-2016-3.core (:require [clojure.string]))
+
+(time (->>
+    (slurp "triangles.txt")
+    clojure.string/split-lines
+    (map (fn [line] (read-string (str "[" line "]"))))
+    (filter (fn [lengths] (let [m (apply max lengths) s (apply + lengths)] (> s (+ m m)))))
+    count
+    (str "Answer 1: ")
+    println))
+
+(time (->>
+    (slurp "triangles.txt")
+    clojure.string/split-lines
+    (map (fn [line] (read-string (str "[" line "]"))))
+    (apply concat)
+    ((fn [v] (concat (take-nth 3 v) (take-nth 3 (rest v)) (take-nth 3 (rest (rest v))))))
+    (partition 3)
+    (filter (fn [lengths] (let [m (apply max lengths) s (apply + lengths)] (> s (+ m m)))))
+    count
+    (str "Answer 2: ")
+    println))
