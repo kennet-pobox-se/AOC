56e448afcaf37da39e1f576823637a3d01c6f41b
Kennet Persson <kennet@pobox.se>
2016-12-07 22:09:07 +0100

    2:a decembers 2:a uppgift löst. Gjorde om 1:a här också.

--- 2016/2/core.clj
+++ 2016/2/core.clj
@@ -1,26 +1,48 @@
 (ns AOC-2016-2.core (:require [clojure.string]))
 
-(def instructions (slurp "instructions.txt"))
+(def keypad-1 {
+    [0 0] \1 [1 0] \2 [2 0] \3
+    [0 1] \4 [1 1] \5 [2 1] \6
+    [0 2] \7 [1 2] \8 [2 2] \9
+})
 
-(def lines (clojure.string/split-lines instructions))
+(defn move [[[x y :as old-pos] keypad] m]
+    (let [new-pos (case m
+        \U [x (dec y)]
+        \D [x (inc y)]
+        \L [(dec x) y]
+        \R [(inc x) y])]
+        [(if (contains? keypad new-pos) new-pos old-pos) keypad]))
 
-(defn move [[x y] m]
-    (case m
-        \U [x (max 0 (dec y))]
-        \D [x (min 2 (inc y))]
-        \L [(max 0 (dec x)) y]
-        \R [(min 2 (inc x)) y]))
+(defn line [[[x y] keypad] l]
+    (reduce move [[x y] keypad] l))
 
-(defn line [[x y] l]
-    (reduce move [x y] l))
+(time (->>
+    (slurp "instructions.txt")
+    clojure.string/split-lines
+    (reductions line [[1 1] keypad-1])
+    (map first)
+    rest
+    (map #(get keypad-1 %1))
+    (apply str)
+    (str "Answer 1: ")
+    println))
 
-(defn code [[x y]]
-    (+ y y y x 1))
+(def keypad-2 {
+                      [2 0] \1
+             [1 1] \2 [2 1] \3 [3 1] \4
+    [0 2] \5 [1 2] \6 [2 2] \7 [3 2] \8 [4 2] \9
+             [1 3] \A [2 3] \B [3 3] \C
+                      [2 4] \D
+})
 
-(def answer-1 (apply str (map code (rest (reductions line [1 1] lines)))))
-
-
-(println (str "Answer 1: " answer-1))
-
-
-    
+(time (->>
+    (slurp "instructions.txt")
+    clojure.string/split-lines
+    (reductions line [[1 1] keypad-2])
+    (map first)
+    rest
+    (map #(get keypad-2 %1))
+    (apply str)
+    (str "Answer 2: ")
+    println))
