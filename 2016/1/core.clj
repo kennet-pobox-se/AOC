2e6b681dfaf20b3e72c55fad7a066d27cb10ebde
Kennet Persson <kennet@pobox.se>
2016-12-07 21:32:48 +0100

    1:a decembers 2:a uppgift  löst. Ny implementation för 1:a uppgiften.

--- 2016/1/core.clj
+++ 2016/1/core.clj
@@ -1,39 +1,44 @@
 (ns AOC-2016-1.core (:require [clojure.string]))
 
-(def instructions (slurp "instructions.txt"))
-
-(defn turn [m dx dy]
-    (case (first m)
+(defn turn [to [dx dy]]
+    (case to
      \R [dy (- dx)]
      \L [(- dy) dx]))
 
 (defn dist [m] (int (subs m 1)))
 
-(defn move [[dx dy x y] m]
-    (let [[ndx ndy] (turn m dx dy)
-          d (dist m)]
-        [ndx ndy (+ x (* d ndx)) (+ y (* d ndy))]))
+(defn follow [[last-leg facing directions]]
+    (if-let [direction (first directions)]
+        (let [[dx dy :as new-facing] (turn (first direction) facing)
+              d (dist direction)]
+            [(loop [leg []
+                    [x y] (last last-leg)
+                    n d]
+                (if (zero? n)
+                    leg
+                    (let [pos [(+ x dx) (+ y dy)]]
+                        (recur (conj leg pos) pos (dec n)))))
+            new-facing
+            (rest directions)])))
 
-(def end (reduce move [0 1 0 0] (clojure.string/split instructions #", ")))
+(defn trace [start facing directions]
+    (apply concat (take-while some? (map first (iterate follow [(list start) facing directions])))))
 
 (defn abs [n] (max n (- n)))
 
-(defn taxicab-distance [x y] (+ (abs x) (abs y)))
+(defn taxicab-distance [[x y]] (+ (abs x) (abs y)))
 
-(println (str "Answer 1: " (let [[_ _ x y] end] (taxicab-distance x y))))
+(time (->> (clojure.string/split (slurp "instructions.txt") #", ") ; Read the text file into a vector of instructions
+    (trace [0 0] [0 1])
+    last ; go to the end
+    taxicab-distance
+    (str "Answer 1: ")
+    println))
 
-(defn move-until-back [[dx dy x y visited] m]
-    (let [[ndx ndy nx ny] (move [dx dy x y] m)]
-        (println visited)
-        (if (contains? visited [nx ny])
-         (reduced [ndx ndy nx ny visited])
-         [ndx ndy nx ny (conj visited [nx ny])])))
-
-(def end-2 (reduce move-until-back [0 1 0 0 #{}] (clojure.string/split instructions #", ")))
-
-(println (str "Answer 2: " (let [[_ _ x y] end-2] (taxicab-distance x y))))
-
-         
-
-
-
+(time (->> (clojure.string/split (slurp "instructions.txt") #", ") ; Read the text file into a vector of instructions
+    (trace [0 0] [0 1])
+    (reduce (fn [visited pos] (if (contains? visited pos) (reduced pos) (conj visited pos))) #{}) ; stop at first visited position
+    taxicab-distance
+    (str "Answer 2: ")
+    println))
+    
