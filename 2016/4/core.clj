214745c3de3442035bc20d7033aeb7df572429c9
Kennet Persson <kennet@pobox.se>
2016-12-12 23:08:54 +0100

    4:e dagen löst

--- 2016/4/core.clj
+++ 2016/4/core.clj
@@ -2,18 +2,25 @@
 
 (with-test 
     (defn checksum [name]
-        (apply str (take 5 (map first (sort (fn [[a b] [c d]] (compare [(- b) a] [(- d) c])) (frequencies name))))))
- (is (= "abcde" (checksum "abcdefg"))))
+        (apply str (take 5 (map first (sort (fn [[a b] [c d]] (compare [(- b) a] [(- d) c])) (dissoc (frequencies name) \-))))))
+ (is (= "abcde" (checksum "efg-abcd"))))
     
+(with-test
+    (defn parse-room [room]
+        (let [[_ encrypted _ id checksum] (re-matches #"((\w+-?)+)-(\d+)\[(\w{5})\]" room)]
+            ;(println (str room ": " encrypted " " id " " checksum))
+            {:encrypted encrypted :id (int id) :checksum checksum}))
+    (is (= {:encrypted "aaaaa-bbb-z-y-x" :id 123 :checksum "abxyz"} (parse-room "aaaaa-bbb-z-y-x-123[abxyz]"))))
 
 (with-test
- (defn room->id [room]
-     (let [parts (filter seq (str/split room #"[-\[\]]"))]
-         (let [encrypted-name (apply str (take (- (count parts) 2) parts))
-               sector-id (int (last (butlast parts)))
-               checksum-maybe (last parts)]
-          (if (= checksum-maybe (checksum encrypted-name))
-           sector-id))))
+    (defn valid-room? [{encrypted :encrypted checksum? :checksum}]
+    (= checksum? (checksum encrypted)))
+    (is (valid-room? (parse-room "not-a-real-room-404[oarel]"))))
+
+(with-test
+    (defn room->id [room]
+        (let [parsed (parse-room room)]
+            (if (valid-room? parsed) (:id parsed))))
  
  (is (= 123 (room->id "aaaaa-bbb-z-y-x-123[abxyz]")))
  (is (= 987 (room->id "a-b-c-d-e-f-g-h-987[abcde]")))
@@ -26,8 +33,39 @@
                             (map room->id)
                             (filter some?))))))
                             
- 
-(run-tests)
+(with-test
+    (defn decrypt [{encrypted :encrypted key :id :as room}]
+        (assoc room :name (apply str (map (fn [c] (if (= \- c) \space (char (+ \a (rem (+ (- c \a) key) 26))))) encrypted))))
+    (is (= "very encrypted name" (:name (decrypt (parse-room "qzmt-zixmtkozy-ivhz-343[xxxxx]"))))))
 
+(time (run-tests))
 
+(time (->> (slurp "rooms.txt")
+    str/split-lines
+    (map room->id)
+    (filter some?)
+    (reduce +)
+    (str "Answer 1: ")
+    println))
+
+(time (->> (slurp "rooms.txt")
+    str/split-lines
+    (map parse-room)
+    (filter valid-room?)
+    (map :id)
+    (reduce +)
+    (str "Answer 1 again: ")
+    println))
+
+(time (->> (slurp "rooms.txt")
+    str/split-lines
+    (map parse-room)
+    (filter valid-room?)
+    (map decrypt)
+    ;(map println)
+    (filter #(= "northpole object storage" (:name %1)))
+    (map :id)
+    (map #(str "Answer 2: " %1))
+    (map println)
+    doall))
 
